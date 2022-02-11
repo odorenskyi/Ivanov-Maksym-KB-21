abfc1f45da5dd4f78941c4b2837efc889aac5fba
Max <axioma162@gmail.com>
2022-02-11 22:13:32 +0200

    ModulesIvanov.h added

---
+++ lab08/prj/ModulesIvanov.h
@@ -0,0 +1,11 @@
+#ifndef MODULESIVANOV_H_INCLUDED
+#define MODULESIVANOV_H_INCLUDED
+
+double c_calculation(double x, double y)
+{
+    double S;
+        S = pow(x, 2) - pow(y, 3) + sqrt(pow(y, x) - M_PI);
+    return S;
+}
+
+#endif // MODULESIVANOV_H_INCLUDED
